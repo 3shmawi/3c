6c9929c2e8c074572709c19c2cf6dbc749874b2c
ashmawi <100779215+3shmawi@users.noreply.github.com>
2024-11-09 15:10:49 +0200

    state management

--- lib/view_model/country_ctrl.dart
+++ lib/view_model/country_ctrl.dart
@@ -5,30 +5,41 @@ import 'package:flutter_children_course/model/country.dart';
 
 class CountryCtrl extends Cubit<CountryStates> {
   CountryCtrl() : super(CountryInitialState());
+
 //super   for   father    and this for    current scope
   final _dio = Dio();
 
   List<CountryModel> data = [];
   List<CountryModel> results = [];
+  Map<String, int> alphabetIndex = {};
 
   void getData() {
     emit(CountryDataLoading());
     _dio.get('https://restcountries.com/v3.1/all').then((response) {
       final countries = response.data;
       data.clear();
-      for (var country in countries) {
-        data.add(CountryModel.fromJson(country));
+      for (int i = 0; i < countries.length; i++) {
+        data.add(CountryModel.fromJson(countries[i]));
       }
 
       data.sort((a, b) => a.name.compareTo(b.name));
+
+      for (int i = 0; i < data.length; i++) {
+        bool isNewChar = (i == 0) || (data[i].name[0] != data[i - 1].name[0]);
+        if (isNewChar) {
+          alphabetIndex[data[i].name[0]] = i;
+        }
+      }
       emit(CountryDataLoaded());
     }).catchError((error) {
       emit(CountryDataFailed(error.toString()));
       print('Failed to load data: $error');
+      throw error;
     });
   }
 
   final searchCtrl = TextEditingController();
+
   void search() {
     emit(CountryDataLoading());
     results = data
@@ -54,5 +65,6 @@ class CountryDataLoaded extends CountryStates {}
 
 class CountryDataFailed extends CountryStates {
   final String error;
+
   CountryDataFailed(this.error);
 }
--- lib/views/api/countries.dart
+++ lib/views/api/countries.dart
@@ -5,6 +5,7 @@ import 'package:flutter_children_course/app/usecase.dart';
 import 'package:flutter_children_course/model/country.dart';
 import 'package:flutter_children_course/view_model/country_ctrl.dart';
 import 'package:flutter_children_course/view_model/theme_ctrl.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 //url launcher
@@ -19,14 +20,19 @@ class _CountriesViewState extends State<CountriesView> {
   final ScrollController _scrollController = ScrollController();
   int highlightedIndex = -1;
 
-  void scrollToItem(int index) {
+  void scrollToItem(int index, [int? count]) {
     setState(() {
       highlightedIndex = index; // Set the highlighted index
+      selectedAlphabetIndex = (count ?? 1) - 1;
     });
 
+    double position = index * 100.0;
+    if (count != null) {
+      position += count * 20;
+    }
     // Animate to the item's position in the list
     _scrollController.animateTo(
-      index * 90.0, // Assuming each item has a height of 50.0
+      position, // Assuming each item has a height of 50.0
       duration: const Duration(seconds: 1),
       curve: Curves.easeInOut,
     );
@@ -37,6 +43,39 @@ class _CountriesViewState extends State<CountriesView> {
         highlightedIndex = -1; // Reset highlight
       });
     });
+  }
+
+  int selectedAlphabetIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll); // Add listener
+  }
+
+  void _onScroll() {
+    // Find the closest alphabet index based on scroll position
+    int closestIndex = 0;
+    double minDistance = double.infinity;
+
+    final cubit = context.read<CountryCtrl>();
+    for (var entry in cubit.alphabetIndex.entries) {
+      int index = entry.value;
+      double offset = index * 90.0; // Approximate height of each section
+      double distance = (_scrollController.offset - offset).abs();
+
+      if (distance < minDistance) {
+        minDistance = distance;
+        closestIndex = cubit.alphabetIndex.keys.toList().indexOf(entry.key);
+      }
+    }
+
+    // Update selected alphabet index if it has changed
+    if (selectedAlphabetIndex != closestIndex) {
+      setState(() {
+        selectedAlphabetIndex = closestIndex;
+      });
+    }
   }
 
   @override
@@ -54,7 +93,7 @@ class _CountriesViewState extends State<CountriesView> {
             },
             icon: const Icon(
               Icons.refresh,
-              color: Colors.cyan,
+              color: Colors.black,
             ),
           ),
           const Icon(
@@ -91,17 +130,81 @@ class _CountriesViewState extends State<CountriesView> {
               errorText: state.error,
             );
           }
-          final countries = context.read<CountryCtrl>().data;
+          final cubit = context.read<CountryCtrl>();
+          final countries = cubit.data;
 
           if (countries.isEmpty) {
             return const AppStatus(CaseStates.empty);
           }
 
-          return ListView.builder(
-            controller: _scrollController,
-            itemCount: countries.length,
-            itemBuilder: (context, index) => _Item(countries[index], false,
-                highlightedIndex == index ? Colors.cyan : theme.dividerColor),
+          return Column(
+            children: [
+              SizedBox(
+                height: 50,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(
+                      cubit.alphabetIndex.length,
+                      (index) => TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: selectedAlphabetIndex == index
+                              ? Colors.cyan
+                              : null,
+                          foregroundColor: selectedAlphabetIndex == index
+                              ? theme.dividerColor
+                              : null,
+                        ),
+                        onPressed: () {
+                          scrollToItem(
+                              cubit.alphabetIndex[
+                                  cubit.alphabetIndex.keys.elementAt(index)]!,
+                              index + 1);
+                        },
+                        child: Text(
+                          cubit.alphabetIndex.keys.elementAt(index),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Scrollbar(
+                  controller: _scrollController,
+                  thickness: 8,
+                  radius: const Radius.circular(5),
+                  interactive: true,
+                  thumbVisibility: true,
+                  child: AnimationLimiter(
+                    child: ListView.builder(
+                      physics: FastScrollPhysics(),
+                      controller: _scrollController,
+                      itemCount: countries.length,
+                      itemBuilder: (context, index) =>
+                          AnimationConfiguration.staggeredList(
+                        position: index,
+                        duration: const Duration(milliseconds: 375),
+                        child: SlideAnimation(
+                          verticalOffset: 50.0,
+                          child: FadeInAnimation(
+                            child: _Item(
+                              countries[index],
+                              false,
+                              highlightedIndex == index
+                                  ? Colors.cyan
+                                  : theme.dividerColor,
+                              cubit.alphabetIndex[countries[index].name[0]] ==
+                                  index,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           );
         },
       ),
@@ -120,6 +223,104 @@ class _CountriesViewState extends State<CountriesView> {
   void dispose() {
     _scrollController.dispose();
     super.dispose();
+  }
+}
+
+class _Item extends StatelessWidget {
+  const _Item(
+    this.country,
+    this.isBottomSheet,
+    this.shadowColor,
+    this.isShowChar,
+  );
+
+  final CountryModel country;
+  final bool isBottomSheet;
+  final Color shadowColor;
+  final bool isShowChar;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        if (isShowChar)
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, right: 15, left: 15),
+            child: Row(
+              children: [
+                const Expanded(
+                  child: Divider(
+                    thickness: 4,
+                  ),
+                ),
+                Text(
+                  " ${country.name[0]} ",
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Expanded(
+                  flex: 20,
+                  child: Divider(),
+                ),
+              ],
+            ),
+          ),
+        GestureDetector(
+          onTap: () {
+            if (isBottomSheet) {
+              Navigator.of(context)
+                  .pop(context.read<CountryCtrl>().getIndex(country.name));
+            } else {
+              _launchUrl(country.mapUrl);
+            }
+          },
+          child: Stack(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                height: 90,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.cyan, width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Card(
+                  margin: EdgeInsets.zero,
+                  elevation: 20,
+                  shadowColor: shadowColor,
+                  color: shadowColor == Colors.cyan ? Colors.white60 : null,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      subtitle: Text(
+                        country.mapUrl,
+                        style: const TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      title: Text(country.name),
+                      trailing: Image.network(
+                        country.flag,
+                        width: 40,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _launchUrl(String u) async {
+    final Uri url = Uri.parse(u);
+
+    if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+      throw Exception('Could not launch $u');
+    }
   }
 }
 
@@ -160,7 +361,7 @@ class BottomSheet extends StatelessWidget {
               return ListView.separated(
                 itemCount: countries.length,
                 itemBuilder: (context, index) =>
-                    _Item(countries[index], true, theme),
+                    _Item(countries[index], true, theme, false),
                 separatorBuilder: (context, index) => const Divider(
                   color: Colors.cyan,
                 ),
@@ -173,67 +374,12 @@ class BottomSheet extends StatelessWidget {
   }
 }
 
-class _Item extends StatelessWidget {
-  const _Item(this.country, this.isBottomSheet, this.shadowColor);
+//
 
-  final CountryModel country;
-  final bool isBottomSheet;
-  final Color shadowColor;
-
+class FastScrollPhysics extends ClampingScrollPhysics {
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        if (isBottomSheet) {
-          Navigator.of(context)
-              .pop(context.read<CountryCtrl>().getIndex(country.name));
-        } else {
-          _launchUrl(country.mapUrl);
-        }
-      },
-      child: Stack(
-        children: [
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            height: 90,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.cyan, width: 2),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Card(
-              margin: EdgeInsets.zero,
-              elevation: 20,
-              shadowColor: shadowColor,
-              color: shadowColor == Colors.cyan ? Colors.white60 : null,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  subtitle: Text(
-                    country.mapUrl,
-                    style: const TextStyle(
-                      color: Colors.blue,
-                    ),
-                  ),
-                  title: Text(country.name),
-                  trailing: Image.network(
-                    country.flag,
-                    width: 40,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Future<void> _launchUrl(String u) async {
-    final Uri url = Uri.parse(u);
-
-    if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
-      throw Exception('Could not launch $u');
-    }
+  double applyPhysicsToUserOffset(ScrollMetrics position, double offset) {
+    // Adjust the multiplier as needed to control scroll speed
+    return offset * 2; // 2x speed
   }
 }
-//
