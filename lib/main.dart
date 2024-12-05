48835cf1ee225860d60ccab97c4bb9dba7b69d52
ashmawi <100779215+3shmawi@users.noreply.github.com>
2024-12-05 18:55:38 +0200

    hamza app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_children_course/services/local_storage.dart';
+import 'package:flutter_children_course/home.dart';
 
-import 'app/app.dart';
-
-//MVVM  Model  View  VIEWMODEL
-//MVC   Model  View  Controller
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SharedPref.init();
-  runApp(const MyApp());
+//MaterialApp التطبيق كامل فيه واحده بس من
+//Scaffold كل بداية صفحة في التطبيق
+//AppBar, Text, Icon, IconButton
+//Container
+void main() {
+  runApp(const HamzaApp());
 }
+
+class HamzaApp extends StatelessWidget {
+  const HamzaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+/*
+Container(
+a:sdf,
+
+),
+ */
